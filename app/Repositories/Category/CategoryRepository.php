2571a6c4d49bfa80de01fd6c9a9aa34fcfa6f266
miguelDR05 <88801050+miguelDR05@users.noreply.github.com>
2025-07-09 17:57:16 -0500

    Crud de categorias avanzado

---
+++ app/Repositories/Category/CategoryRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories\Category;
+
+use App\Models\Category;
+
+class CategoryRepository
+{
+    public function all()
+    {
+        return Category::all();
+    }
+
+    public function find($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    public function create(array $data): Category
+    {
+        return Category::create($data);
+    }
+
+    public function update(Category $category, array $data): Category
+    {
+        $category->update($data);
+        return $category;
+    }
+
+    public function delete(Category $category): bool
+    {
+        return $category->delete();
+    }
+}
